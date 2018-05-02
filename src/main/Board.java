7b68a2c6b7cdd76b387d6cf68a17f728a6b9b96a
Khambar Dussaliyev <anuarkaliyev23@gmail.com>
2018-05-02 13:17:05 +0600

    Fixed idiot bug with incorrect y

--- src/main/Board.java
+++ src/main/Board.java
@@ -27,17 +27,20 @@ public class Board {
     }
 
     private void checkCoordinatesInBounds(int coordinate) {
-        if (coordinate < 0 || coordinate >= BOARD_SIZE) throw new OutOfBoundCoordinatesException();
+        if (coordinate < 0 || coordinate > BOARD_SIZE) throw new OutOfBoundCoordinatesException();
     }
 
     private void placeAllBoats() {
-        int count = 1;
-        for (int i = MAX_BOAT_SIZE; i > 0; i--) { //size
-            for (int j = 0; j < count; j++) {
-                placeBoat(i);
-            }
-            count++;
-        }
+        placeBoat(4);
+        placeBoat(3);
+        placeBoat(3);
+        placeBoat(2);
+        placeBoat(2);
+        placeBoat(2);
+        placeBoat(1);
+        placeBoat(1);
+        placeBoat(1);
+        placeBoat(1);
     }
 
     private boolean isEdgeCoordinate(int coordinate) {
@@ -45,117 +48,73 @@ public class Board {
         return (coordinate == 0 || coordinate == BOARD_SIZE - 1);
     }
 
+    private boolean checkNeighbours(int x, int y) {
+        checkCoordinatesInBounds(x);
+        checkCoordinatesInBounds(y);
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                try {
+                    if (board[i][j]) {
+                        return false;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    //Do nothing
+                }
+            }
+        }
+        return true;
+    }
+
     private boolean isAvailableForPlacement(int x, int y) {
         checkCoordinatesInBounds(x);
         checkCoordinatesInBounds(y);
 
-        if (isEdgeCoordinate(x) && !isEdgeCoordinate(y)) {
-            return isAvailableForPlacementEdgeX(x, y);
-        } else if (!isEdgeCoordinate(x) && isEdgeCoordinate(y)) {
-            return isAvailableForPlacementEdgeY(x, y);
-        } else if (isEdgeCoordinate(x) && isEdgeCoordinate(y)) {
-            return isAvailableForPlacementEdgeBoth(x, y);
-        } else {
-            return isAvailableForPlacementMiddleBoth(x, y);
-        }
-    }
-
-    private boolean isAvailableForPlacementEdgeX(int x, int y) {
-        if (!isEdgeCoordinate(x)) throw new UnexpectedNotEdgingCoordinatesExceptions();
-        if (isEdgeCoordinate(y)) throw new UnexpectedEdgingCoordinatesException();
-
-        int offset;
-        if (x == 0) {
-            offset = 1;
-        } else {
-            offset = -1;
-        }
-
-        return !(board[x][y] || board[x][y + 1] || board[x + offset][y + 1] || board[x + offset][y] || board[x + offset][y - 1] || board[x][y - 1]);
-    }
-
-    private boolean isAvailableForPlacementEdgeY(int x, int y) {
-        if (!isEdgeCoordinate(y)) throw new UnexpectedNotEdgingCoordinatesExceptions();
-        if (isEdgeCoordinate(x)) throw new UnexpectedEdgingCoordinatesException();
-
-        int offset;
-        if (y == 0) {
-            offset = 1;
-        } else {
-            offset = -1;
-        }
-
-        return !(board[x][y] || board[x - 1][y] || board[x - 1][y + offset] || board[x][y + offset] || board[x + 1][y + offset] || board[x + 1][y]);
-    }
-
-    private boolean isAvailableForPlacementEdgeBoth(int x, int y) {
-        if (!isEdgeCoordinate(x) || !isEdgeCoordinate(y)) throw new UnexpectedNotEdgingCoordinatesExceptions();
-
-        if (x == 0 && y == 0) {
-            return !(board[x][y] || board[x + 1][y] || board[x + 1][y + 1] || board[x][y + 1]);
-        } else if (x == 0 && y == BOARD_SIZE - 1) {
-            return !(board[x][y] || board[x + 1][y] || board[x + 1][y - 1] || board[x][y - 1]);
-        } else if (x == BOARD_SIZE - 1 && y == 0) {
-            return !(board[x][y] || board[x - 1][y] || board[x - 1][y + 1] || board[x][y + 1]);
-        } else {
-            return !(board[x][y] || board[x - 1][y] || board[x - 1][y - 1] || board[x][y - 1]);
-        }
+        return checkNeighbours(x, y);
     }
 
 
-    private boolean isAvailableForPlacementMiddleBoth(int x, int y) {
-        if (isEdgeCoordinate(x) || isEdgeCoordinate(y)) throw new UnexpectedEdgingCoordinatesException();
-
-        for (int i = x - 1; i <= x + 1; i++) {
-            for (int j = y - 1; j <= y + 1; j++) {
-                if (board[i][j]) return false;
-            }
-        }
-        return true;
-    }
 
     private void placeBoat(int size) {
         if (size < 0 || size > MAX_BOAT_SIZE) throw new InvalidBoatSizeException();
 
+        System.out.println("Iteration");
         int x = ThreadLocalRandom.current().nextInt(BOARD_SIZE);
         int y = ThreadLocalRandom.current().nextInt(BOARD_SIZE);
 
-        boolean[] available = new boolean[size];
-        boolean horizontal = true;
+        boolean availableFlag = true;
+        boolean vertical;
 
         if (x + size < BOARD_SIZE) {
-            horizontal = true;
+            vertical = true;
             for (int i = x; i < x + size; i++) {
-                available[i - x] = isAvailableForPlacement(i, y);
+                if (!isAvailableForPlacement(i, y)) availableFlag = false;
             }
         } else if (y + size < BOARD_SIZE) {
-            horizontal = false;
+            vertical = false;
             for (int j = y; j < y + size; j++) {
-                available[j - y] = isAvailableForPlacement(x, j);
+                if (!isAvailableForPlacement(x, j)) availableFlag = false;
+            }
+        } else {
+            vertical = false;
+            placeBoat(size);
+        }
+
+        if (availableFlag) {
+            if (vertical) {
+                for (int i = x; i < x + size; i++) {
+                    System.out.println("i : " + i);
+                    board[i][y] = true;
+                }
+            } else {
+                for (int j = y; j < y + size; j++) {
+                    board[x][j] = true;
+                }
             }
         } else {
             placeBoat(size);
         }
-
-        if (allArrayElementsAreTrue(available)) {
-            if (horizontal) {
-                for (int i = x; i < x + size; i++) {
-                    board[i][y] = true;
-                }
-            } else {
-                for (int j = 0; j < y + size; j++) {
-                    board[x][j] = true;
-                }
-            }
-        }
     }
 
-    private boolean allArrayElementsAreTrue(boolean[] values) {
-        for (boolean value : values) {
-            if (!value) return false;
-        }
-        return true;
-    }
 
     public boolean[][] getBoard() {
         return board;
